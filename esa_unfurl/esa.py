f11776f93819bced1857307da87e843ebad1979f
reiyw <reiyw.setuve@gmail.com>
2019-02-11 22:13:51 +0900

    init

---
+++ esa_unfurl/esa.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+from typing import Tuple
+from urllib.parse import urlparse
+
+import requests
+
+
+def parse_url(url: str) -> Tuple[str, int]:
+    o = urlparse(url)
+    return o.netloc.split(".")[0], int(o.path.split("/")[-1])
+
+
+def truncate_text(text: str) -> str:
+    lines = text.split("\n")
+    n_chars = 0
+    truncated_lines = []
+    for line in lines:
+        if n_chars > 400:
+            break
+        n_chars += len(line)
+        truncated_lines.append(line)
+    return "\n".join(truncated_lines)
+
+
+@dataclass
+class EsaPage:
+    title: str
+    url: str
+    text: str
+
+    @classmethod
+    def request(cls, url: str, token: str) -> "EsaPage":
+        team_name, post_number = parse_url(url)
+        api_url = f"https://api.esa.io/v1/teams/{team_name}/posts/{post_number}"
+        headers = {"Authorization": f"Bearer {token}"}
+
+        r = requests.get(api_url, headers=headers)
+        data = r.json()
+        title = data["full_name"]
+        text = data["body_md"]
+
+        return cls(title, url, text)
+
+    def to_attachment(self):
+        return {
+            "title": self.title,
+            "title_link": self.url,
+            "color": "#0a9b94",
+            "text": truncate_text(self.text),
+        }
